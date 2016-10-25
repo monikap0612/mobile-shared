a8d791640bf8120a49091aeb318e7eeb87cb4d2f
iggor_kurr <kurinnoyi@gmail.com>
2016-10-25 17:42:04 +0300

    Added i18n
    * Added basic translations from tech_v3 app
    * Created helpers for making translations and its setup

---
+++ src/translations/index.js
@@ -0,0 +1,21 @@
+import en from './en';
+import fr from './fr';
+import ru from './ru';
+import ro from './ro';
+import he from './he';
+import es from './es';
+import it from './it';
+import de from './de';
+
+export const translations = {
+  en,
+  fr,
+  ru,
+  ro,
+  he,
+  es,
+  it,
+  de
+}
+
+export default translations
